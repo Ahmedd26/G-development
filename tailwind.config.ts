adf3bab54a369b12afb96716facf30802237db3b
Ahmedd26 <ahmedabdellatife1@gmail.com>
2025-02-20 03:59:40 +0200

    re-adjusted the max-w-container to fit design need, this will allow the container after having the 16px padding (32px from both two sides) to add up to 1400px as the previous setup which did not take inline padding into account. Added the gray color used in many section titles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,24 @@
 import type { Config } from "tailwindcss";
 
 export default {
-    content: [
-        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-        "./components/**/*.{js,ts,jsx,tsx,mdx}",
-        "./app/**/*.{js,ts,jsx,tsx,mdx}",
-        "./.storybook/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-        extend: {
-            fontFamily: {
-                helvetica: "var(--font-helvetica-now), sans-serif",
-            },
-            maxWidth: {
-                container: "1400px",
-            },
-        },
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./.storybook/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        helvetica: "var(--font-helvetica-now), sans-serif",
+      },
+      maxWidth: {
+        container: "1368px",
+      },
+      colors: {
+        "design-gray": "#777777",
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 } satisfies Config;
