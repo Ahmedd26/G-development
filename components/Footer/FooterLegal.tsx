243218c26d483aa879de3cdcbc1d349636e0a5ad
Ahmedd26 <ahmedabdellatife1@gmail.com>
2025-02-21 17:52:01 +0200

    Added spacing @FooterLegal Component for desktop screen size

--- components/Footer/FooterLegal.tsx
+++ components/Footer/FooterLegal.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function FooterLegal() {
   return (
     <div className="flex flex-col gap-6 md:flex-row md:justify-between md:pt-5">
-      <div className="flex flex-col gap-2 md:flex-row-reverse md:justify-start">
-        <div className="flex items-center gap-3">
+      <div className="flex flex-col gap-2 md:flex-row-reverse md:justify-start md:gap-10">
+        <div className="flex items-center gap-3 md:gap-10">
           <Link href="/" className="text-xs font-normal md:text-base">
             Privacy
           </Link>
